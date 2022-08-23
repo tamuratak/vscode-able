6ff3f52a26e3e6137ffe6491b59b105a0f7cb403
Takashi Tamura <tamuratak@users.noreply.github.com>
2022-08-23 15:45:28 +0900

    Add a tweak

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import * as vscode from 'vscode'
 
+export function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 export function activate() {
     let terminalIsVisible = false
     vscode.commands.registerCommand('able.toggleTerminalAndSideBar', async () => {
         await vscode.commands.executeCommand('workbench.action.toggleSidebarVisibility')
         if (terminalIsVisible) {
             vscode.window.activeTerminal?.show()
+            await sleep(10)
             await vscode.commands.executeCommand('workbench.action.terminal.toggleTerminal')
             terminalIsVisible = false
         } else {
