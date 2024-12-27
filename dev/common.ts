57351ab722f3a0dfc0210eda494d7dd9fc7396e4
Takashi Tamura <tamuratak@users.noreply.github.com>
2024-12-28 07:55:10 +0900

    chore: clean up dependencies and remove unused download functions

--- dev/common.ts
+++ dev/common.ts
@@ -2,44 +2,17 @@
 // Licensed under the MIT License.
 
 import { https } from 'follow-redirects';
-import { HttpsProxyAgent } from 'https-proxy-agent';
-import { getProxyForUrl } from 'proxy-from-env';
-import { parse } from 'url';
-import { spawnSync } from 'child_process';
-import { Presets, SingleBar } from 'cli-progress';
-import * as fs from 'fs';
-import { platform } from 'os';
-import * as path from 'path';
-import * as tar from 'tar';
-import * as unzipper from 'unzipper';
-
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const decompress = require('decompress');
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const decompressTarbz = require('decompress-tarbz2');
+import { parse } from 'node:url';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 export const PYTHON_VERSION = '3.12.1'; // Version of Python used in Pyodide, check https://github.com/pyodide/pyodide/blob/main/Makefile.envs
 export const PYTHON_VERSION_SHORT = '3.12'; // Version of Python used in Pyodide, check https://github.com/pyodide/pyodide/blob/main/Makefile.envs
 export const PYODIDE_VERSION = '0.27.0a2';
 export const PYODIDE_KERNEL_VERSION = '0.4.3';
 
-export function downloadContents(url: string) {
-	return new Promise<string>((resolve, reject) => {
-		let result = '';
-		https.get(getRequestOptions(url), (response) => {
-			if (response.statusCode !== 200) {
-				return reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
-			}
-			response.on('data', (d) => (result += d.toString()));
-			response.on('end', () => resolve(result));
-		});
-	});
-}
-
-
 export function getRequestOptions(url: string) {
 	const token = process.env['GITHUB_TOKEN'];
-	const proxy = getProxyForUrl(url);
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const downloadOpts: Record<string, any> = {
 		headers: {
@@ -53,50 +26,8 @@ export function getRequestOptions(url: string) {
 		downloadOpts['headers'].authorization = `token ${token}`;
 	}
 
-	if (proxy !== '') {
-		Object.assign(downloadOpts, {
-			...downloadOpts,
-			agent: new HttpsProxyAgent(proxy)
-		});
-	}
-
 	return downloadOpts;
 }
-
-
-export async function extractTarBz2(file: string, dir: string) {
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
-	if (platform() === 'win32') {
-		await decompress(file, dir, {
-			plugins: [
-				decompressTarbz()
-			]
-		});
-		return;
-	}
-	try {
-		extractTarBz2UnixShell(file, dir);
-	} catch (ex) {
-		console.error(`Failed to extract using shell scripts`, ex);
-		await decompress(file, dir, {
-			plugins: [
-				decompressTarbz()
-			]
-		});
-	}
-}
-
-function extractTarBz2UnixShell(file: string, target: string) {
-	const command = `tar -xf ${file}`;
-	console.log(`Extracting using shell command ${command}`)
-	const output = spawnSync(command, { shell: true, cwd: target });
-	if (output.error) {
-		throw output.error;
-	}
-}
-
 
 export function downloadFile(url: string, dest: string) {
 	if (fs.existsSync(dest)) {
@@ -116,41 +47,16 @@ export function downloadFile(url: string, dest: string) {
 					return reject(new Error(`Failed to get '${url}' (${response.statusCode})`));
 				}
 
-				const totalBytes = parseInt(response.headers['content-length'] || '0');
-				const bar = new SingleBar({}, Presets.shades_classic);
-				bar.start(100, 0);
 				let receivedBytes = 0;
 				response.on('data', function (chunk) {
 					receivedBytes += chunk.length;
-					const percentage = (receivedBytes * 100) / totalBytes;
-					bar.update(percentage);
 				});
 				response.pipe(file);
 
 				file.on('finish', () => {
-					bar.stop();
 					file.close(() => resolve());
 				});
 			})
 			.on('error', (err) => reject(err));
 	});
-}
-
-
-export async function extractFile(tgzFile: string, extractDir: string) {
-	if (tgzFile.endsWith('.zip')) {
-		const directory = await unzipper.Open.file(tgzFile);
-		await directory.extract({ path: extractDir })
-		if (fs.existsSync(path.join(extractDir, '__MACOSX'))) {
-			fs.rmSync(path.join(extractDir, '__MACOSX'), { recursive: true });
-		}
-		return;
-	}
-	await tar.x({
-		file: tgzFile,
-		cwd: extractDir,
-		'strip-components': 1
-	});
-
-	return extractDir;
 }
--- dev/download.ts
+++ dev/download.ts
@@ -2,15 +2,10 @@
 // Licensed under the MIT License.
 
 import * as fs from 'node:fs';
-import { tmpdir } from 'node:os';
 import * as path from 'node:path';
-import { downloadContents, downloadFile, extractFile, extractTarBz2, PYODIDE_KERNEL_VERSION, PYODIDE_VERSION } from './common.js';
+import { downloadFile } from './common.js';
 
-
-const pydodideKernelApiUrl = `https://api.github.com/repos/jupyterlite/pyodide-kernel/releases/tags/v${PYODIDE_KERNEL_VERSION}`;
-
-const pyodideApiUri = `https://api.github.com/repos/pyodide/pyodide/releases/tags/${PYODIDE_VERSION}`;
-const packagesToKeep = ['attrs-', 'decorator', 'distutils', 'fonttools', 'hashlib',
+export const packagesToKeep = ['attrs-', 'decorator', 'distutils', 'fonttools', 'hashlib',
     'matplotlib', 'micropip', 'numpy', 'mpmpath', 'openssl', 'package.json', 'pandas',
     'pydecimal', 'pyodide', 'python_date', 'python_std', 'pytz', 'six', 'sqlite', 'ssl',
     // These are definitely required to get things running.
@@ -22,58 +17,6 @@ const packagesToKeep = ['attrs-', 'decorator', 'distutils', 'fonttools', 'hashli
     // Do not distribute these due to licensing issues.
     // 'pillow'
 ]
-
-type ReleaseInfo = {
-    assets: {
-        url: string;
-        browser_download_url: string;
-        name: string;
-        content_type: string;
-        size: number;
-    }[];
-};
-
-export async function downloadPyodideKernel() {
-    const contents = await downloadContents(pydodideKernelApiUrl);
-    const json: ReleaseInfo = JSON.parse(contents);
-    const fileToDownload = json.assets.find((asset) =>
-        asset.name.toLowerCase() === `jupyterlite-pyodide-kernel-${PYODIDE_KERNEL_VERSION}.tgz`
-    )!;
-    console.debug(`Download ${fileToDownload.name} (${fileToDownload.url})`);
-    const tarFile = path.join(tmpdir(), fileToDownload.name);
-    if (fs.existsSync(tarFile)) {
-        fs.rmSync(tarFile);
-    }
-    await downloadFile(fileToDownload.url, tarFile);
-    console.debug(`Downloaded to ${tarFile}`);
-    const dir = path.join(__dirname, '..', 'pyodide');
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-    await extractFile(tarFile, dir);
-    await deleteUnwantedFilesFromPyodideKernel(dir);
-    console.debug(`Extracted to ${dir}`);
-}
-
-
-async function deleteUnwantedFilesFromPyodideKernel(dir: string) {
-    const files = [
-        path.join(dir, 'lib'),
-        path.join(dir, 'style'),
-        path.join(dir, 'package.json'),
-        path.join(dir, 'tsconfig.tsbuildinfo')
-    ];
-    files.forEach((file) => {
-        if (fs.existsSync(file)) {
-            fs.rmSync(file, { recursive: true });
-        }
-    });
-    const pypiFiles = fs.readdirSync(path.join(dir, 'pypi'));
-    pypiFiles
-        .filter((file) => file.toLowerCase().startsWith('widgetsnbextension'))
-        .forEach((file) => fs.rmSync(path.join(dir, 'pypi', file), { recursive: true }));
-}
-
 
 export async function downloadCommWheel() {
     const url = 'https://files.pythonhosted.org/packages/e6/75/49e5bfe642f71f272236b5b2d2691cf915a7283cc0ceda56357b61daa538/comm-0.2.2-py3-none-any.whl';
@@ -95,58 +38,11 @@ export async function downloadSeabornWheels() {
     await downloadFile(url, dest);
 }
 
-
-export async function downloadPyodideArtifacts() {
-    const contents = await downloadContents(pyodideApiUri);
-    const json: ReleaseInfo = JSON.parse(contents);
-    const fileToDownload = json.assets.find((asset) =>
-        asset.name.toLowerCase() === `pyodide-${PYODIDE_VERSION}.tar.bz2`
-    )!;
-    console.debug(`Downloading ${fileToDownload.name} (${fileToDownload.url})`);
-    const tarFile = path.join(__dirname, '..', 'temp', fileToDownload.name);
-    if (!fs.existsSync(path.dirname(tarFile))) {
-        fs.mkdirSync(path.dirname(tarFile), { recursive: true });
-    }
-    await downloadFile(fileToDownload.url, tarFile);
-    console.debug(`Downloaded to ${tarFile}`);
-    const dir = path.join(__dirname, '..', 'temp');
-    console.debug(`Extracting into ${dir}`);
-    // Extraction is slow, use the previously extracted files if they exist.
-    if (!fs.existsSync(path.join(dir, 'pyodide')) || !fs.readdirSync(path.join(dir, 'pyodide')).length) {
-        await extractTarBz2(tarFile, dir);
-    }
-
-    // Extract only the files we need.
-    fs.readdirSync(path.join(dir, 'pyodide')).forEach(file => {
-        const dest = path.join(__dirname, '..', 'pyodide', file);
-        const source = path.join(dir, 'pyodide', file);
-        if (file.toLowerCase().endsWith('-tests.tar')) {
-            return;
-        }
-        if (fs.existsSync(dest)) {
-            fs.rmSync(dest, { recursive: true });
-        }
-        if (!fs.existsSync(path.dirname(dest))) {
-            fs.mkdirSync(path.dirname(dest), { recursive: true });
-        }
-
-        if (packagesToKeep.some(keep => file.toLowerCase().startsWith(keep.toLowerCase()))) {
-            if (fs.statSync(source).isDirectory()) {
-                fs.cpSync(source, dest, { recursive: true });
-            } else {
-                fs.copyFileSync(source, dest);
-            }
-        }
-    });
-    console.debug(`Extracted to ${dir}`);
-}
-
 async function main() {
     const pyodideDir = path.join(__dirname, '..', 'pyodide');
     if (fs.existsSync(pyodideDir)) {
         fs.rmSync(pyodideDir, { recursive: true });
     }
-    await downloadPyodideKernel();
     await downloadCommWheel();
     await downloadSeabornWheels();
     // renameLicense();
