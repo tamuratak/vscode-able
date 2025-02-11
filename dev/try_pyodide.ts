f835a345d536887a1bd06df9cbcafe64d752bcbc
Takashi Tamura <tamuratak@users.noreply.github.com>
2025-02-12 07:46:36 +0900

    chore: remove pyodide dependency and update import path to use module alias

--- dev/try_pyodide.ts
+++ dev/try_pyodide.ts
@@ -1,7 +1,8 @@
-import { loadPyodide } from 'pyodide'
+import { loadPyodide } from '#pyodide'
 
 async function helloPython() {
   const pyodide = await loadPyodide()
+  console.log(pyodide)
   const result = await pyodide.runPythonAsync('1+1') as number
   console.log('Python says that 1+1 =', result)
 }
