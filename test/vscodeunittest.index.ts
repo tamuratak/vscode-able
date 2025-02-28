40bc7e80fafadf501bfead937c968c4ebcfb131a
Takashi Tamura <tamuratak@users.noreply.github.com>
2025-02-28 16:27:31 +0900

    feat: add unit tests and test runner for extension functionality

---
+++ test/vscodeunittest.index.ts
@@ -0,0 +1,34 @@
+import * as path from 'node:path'
+import Mocha from 'mocha'
+import {globSync} from 'glob'
+
+export function run(): Promise<void> {
+    // Create the mocha test
+    const mocha = new Mocha({
+        ui: 'tdd',
+        color: true,
+        timeout: 0
+    })
+    const testsRoot = path.resolve(__dirname, '.')
+
+    return new Promise((resolve, reject) => {
+        const files = globSync('vscodeunittest/**/*.test.js', { cwd: testsRoot })
+        // Add files to the test suite
+        files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)))
+
+        try {
+            // Run the mocha test
+            mocha.run(failures => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`))
+                } else {
+                    resolve()
+                }
+            })
+        } catch (e) {
+            console.error(e)
+            // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+            reject(e)
+        }
+    })
+}
