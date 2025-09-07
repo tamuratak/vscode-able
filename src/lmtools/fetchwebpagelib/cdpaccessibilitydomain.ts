5146f58ba367349b2a9258d56766dc54a670108c
Takashi Tamura <tamuratak@users.noreply.github.com>
2025-09-07 15:18:22 +0900

    fix(cdpaccessibilitydomain): improve header cell detection in processTableNode function

--- src/lmtools/fetchwebpagelib/cdpaccessibilitydomain.ts
+++ src/lmtools/fetchwebpagelib/cdpaccessibilitydomain.ts
@@ -1,29 +1,7 @@
-/**
-MIT License
-
-Copyright (c) 2015 - present Microsoft Corporation
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-- https://github.com/microsoft/vscode/blob/main/src/vs/platform/webContentExtractor/test/electron-main/cdpAccessibilityDomain.test.ts
-
-*/
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 //#region Types
 
@@ -471,15 +449,17 @@ export function processTableNode(node: AXNodeTree, buffer: string[]): void {
 	}
 
 	if (rows.length > 0) {
-		const isColumnHeader = (c: AXNodeTree) => {
+		let headerRowIndex = rows.findIndex(r => r.children.some((c) => {
 			const rr = getNodeRole(c.node);
-			return rr === 'columnheader';
-		}
-		let headerRowIndex = rows.findIndex(r => r.children.some(isColumnHeader));
+			return rr === 'columnheader'
+		}));
 		if (headerRowIndex === -1) {
 			headerRowIndex = 0;
 		}
-		const headerCells = rows[headerRowIndex].children.filter(isColumnHeader);
+		const headerCells = rows[headerRowIndex].children.filter((c) => {
+			const rr = getNodeRole(c.node);
+			return rr === 'columnheader' || rr === 'rowheader' || rr === 'cell';
+		});
 
 		// Generate header row
 		const headerContent = headerCells.map(cell => getNodeText(cell.node, false) || ' ');
