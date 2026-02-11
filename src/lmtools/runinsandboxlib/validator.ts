15c26ccf6a0a8e93d91988e0db80cc2d7474381d
Takashi Tamura <tamuratak@users.noreply.github.com>
2026-02-11 20:24:59 +0900

    feat(validator): add 'pwd' command to allowed commands set

--- src/lmtools/runinsandboxlib/validator.ts
+++ src/lmtools/runinsandboxlib/validator.ts
@@ -2,7 +2,7 @@ import path from 'node:path'
 import { collectCommands, CommandNode, hasNoWriteRedirection } from './commandparser.js'
 
 const forbiddenCharacters = /[~]/
-const allowedCommands = new Set(['cat', 'cd', 'echo', 'head', 'ls', 'nl', 'rg', 'printf', 'sed', 'tail', 'grep'])
+const allowedCommands = new Set(['cat', 'cd', 'echo', 'head', 'ls', 'nl', 'rg', 'printf', 'sed', 'tail', 'grep', 'pwd'])
 
 export async function isAllowedCommand(command: string, workspaceRootPath: string | undefined): Promise<boolean> {
     if (forbiddenCharacters.test(command)) {
