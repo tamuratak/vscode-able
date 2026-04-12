8adcfb993eb980f17b06c6a0e0a58251cd919c4e
Takashi Tamura <tamuratak@users.noreply.github.com>
2026-04-12 20:08:05 +0900

    feat(mathjax): integrate MathJax support with worker pool and extensions

---
+++ src/mathjaxlib/mathjaxpool.ts
@@ -0,0 +1,73 @@
+import * as vscode from 'vscode'
+import * as path from 'node:path'
+import * as workerpool from 'workerpool'
+import type { Proxy } from 'workerpool'
+import type { IMathJaxWorker } from './mathjaxpool_worker.js'
+import type { SupportedExtension } from 'mathjax-full'
+
+const supportedExtensionList = [
+    'amscd',
+    'bbox',
+    'boldsymbol',
+    'braket',
+    'bussproofs',
+    'cancel',
+    'cases',
+    'centernot',
+    'colortbl',
+    'empheq',
+    'enclose',
+    'extpfeil',
+    'gensymb',
+    'html',
+    'mathtools',
+    'mhchem',
+    'physics',
+    'textcomp',
+    'textmacros',
+    'unicode',
+    'upgreek',
+    'verb'
+]
+
+export class MathJaxPool {
+    private readonly pool: workerpool.Pool
+    private readonly proxyPromise: workerpool.Promise<Proxy<IMathJaxWorker>>
+
+    constructor() {
+        this.pool = workerpool.pool(
+            path.join(__dirname, 'mathjaxpool_worker.js'),
+            { minWorkers: 1, maxWorkers: 1, workerType: 'thread' }
+        )
+        this.proxyPromise = this.pool.proxy<IMathJaxWorker>()
+        void this.initializeExtensions()
+    }
+
+    async dispose() {
+        await this.pool.terminate(true)
+    }
+
+    private initializeExtensions() {
+        void this.loadExtensions()
+        vscode.workspace.onDidChangeConfiguration(async (ev) => {
+            if (ev.affectsConfiguration('latex-toybox.hover.preview.mathjax.extensions')) {
+                return this.loadExtensions()
+            }
+        })
+    }
+
+    async typeset(arg: string, opts: { scale: number, color: string }): Promise<string> {
+        const proxy = await this.proxyPromise
+        const svgHtml = await proxy.typeset(arg, opts).timeout(3000)
+        return svgHtml
+    }
+
+    private async loadExtensions() {
+        const configuration = vscode.workspace.getConfiguration('latex-toybox')
+        const extensions = configuration.get('hover.preview.mathjax.extensions', []) as SupportedExtension[]
+        const extensionsToLoad = extensions.filter((ex) => supportedExtensionList.includes(ex))
+        const proxy = await this.proxyPromise
+        return proxy.loadExtensions(extensionsToLoad)
+    }
+
+}
