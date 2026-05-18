1f196003c1af3293cb0a6de9a50e44afc1bc1c4a
Takashi Tamura <tamuratak@users.noreply.github.com>
2026-05-19 07:42:45 +0900

    style(chatprovider): add GeminiCliChatProvider to barrel exports

--- src/chatprovider/chatprovider.ts
+++ src/chatprovider/chatprovider.ts
@@ -1,2 +1,3 @@
 export { GeminiChatProvider } from './geminichatprovider/geminichatprovider.js'
+export { GeminiCliChatProvider } from './geminicli/geminiclichatprovider.js'
 export { OpenCodeGoChatModelProvider } from './opencodegochatprovider/provider.js'
