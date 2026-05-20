36f21c7982b43a7bae8421c3984668109eacbe55
Takashi Tamura <tamuratak@users.noreply.github.com>
2026-05-21 06:44:11 +0900

    chore(geminicli): remove geminicli chat provider and related utilities

--- src/chatprovider/chatprovider.ts
+++ src/chatprovider/chatprovider.ts
@@ -1,3 +1,2 @@
 export { GeminiChatProvider } from './geminichatprovider/geminichatprovider.js'
-export { GeminiCliChatProvider } from './geminicli/geminiclichatprovider.js'
 export { OpenCodeGoChatModelProvider } from './opencodegochatprovider/provider.js'
